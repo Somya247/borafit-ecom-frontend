2d9c1583ed3c25b1d39101d43686fe0f240d9c1c
Somya247 <96365969+Somya247@users.noreply.github.com>
2023-08-05 22:55:06 +0000

    added new files nad images and made changes to the buttons

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ def about():
 
 @app.route('/shop')
 def shop():
-    return render_template('about.html')
+    return render_template('shop.html')
 
 @app.route('/blog')
 def blog():
-    return render_template('about.html')
+    return render_template('blog.html')
 
 @app.route('/cart')
 def cart():
-    return render_template('about.html')
+    return render_template('cart.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('about.html')
+    return render_template('contact.html')
+
+@app.route('/sproducts')
+def sproducts():
+    return render_template('sproduct.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True) 
